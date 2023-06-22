c80c09c6b13250394daccc6155076144ef789644
ruslkan <ridik79@gmail.com>
2023-06-23 00:24:24 +0300

    added company details

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,6 +5,14 @@
     style="background-image: url({{ asset('images/header_bg.jpg') }})"
 @endif>
     <div class="footer__inner">
-        <p class="footer__copyright">Copyright &copy;{{ date('Y') }}</p>
+        <div class="footer__data-organization">
+            <ul class="data-organization__list">
+                <li class="data-organization__item">УНП - {{ setting('site.unp_organization') }}</li>
+                <li class="data-organization__item">{{ setting('site.address_organization') }}</li>
+                <li class="data-organization__item">тел.{{ setting('site.phone_main') }}</li>
+                <li class="data-organization__item">тел.{{ setting('site.phone_additional') }}</li>
+            </ul>
+        </div>
     </div>
+    <p class="footer__copyright">Copyright &copy;{{ date('Y') }}</p>
 </footer>
